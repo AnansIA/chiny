921e873e839747276ecf0af3db20923b7f7563e8
AnansIA <anansi.main@gmail.com>
2024-07-08 17:49:15 -0300

    first commit

---
+++ app/models.py
@@ -0,0 +1,73 @@
+from . import db
+
+SHAPES = [
+    'Circular', 'Rectangular', 'Square', 'Oval', 'Triangular', 'Hexagonal',
+    'Octagonal', 'Elliptical', 'Cylindrical', 'Conical', 'Spherical',
+    'Cuboidal', 'Tetrahedral', 'Pyramidal', 'Trapezoidal', 'Parallelogram',
+    'Rhomboidal', 'Star-shaped', 'Heart-shaped', 'Crescent', 'Semi-circular',
+    'Polygonal', 'Irregular', 'Custom'
+]
+
+
+class Cavity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    matriz_id = db.Column(db.Integer,
+                          db.ForeignKey('matrix.id'),
+                          nullable=False)
+    nombre = db.Column(db.String(120), nullable=False)
+    pieza_id = db.Column(db.Integer,
+                         db.ForeignKey('piece.id'),
+                         nullable=False)
+
+
+class MeasuresMatrix(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    width_cm = db.Column(db.Integer, nullable=False)
+    height_cm = db.Column(db.Integer, nullable=False)
+    measuresmatrix = db.relationship('Matrix',
+                                     backref='measuresmatrix',
+                                     lazy=True)
+
+
+class Matrix(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    identifier = db.Column(db.String(5), nullable=False)
+    measuresmatrix = db.Column(db.Integer,
+                                db.ForeignKey('measuresmatrix.id'),
+                                nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    weight_unit_grs = db.Column(db.Float, nullable=False)
+    with_move = db.Column(db.Boolean, default=False)
+    is_combinable = db.Column(db.Boolean, default=True)
+    cavities = db.relationship('Cavity', backref='matrix', lazy=True)
+
+
+class Holder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    identifier = db.Column(db.String(5), nullable=False)
+    width_cm = db.Column(db.Integer, nullable=False)
+    height_cm = db.Column(db.Integer, nullable=False)
+    weight_grs = db.Column(db.Float, default=0)
+    with_move = db.Column(db.Boolean, default=False)
+    available = db.Column(db.Boolean, default=True)
+
+
+class Machine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brand_machine = db.Column(db.String(120), nullable=False)
+    ton = db.Column(db.Integer, nullable=False)
+
+
+class Piece(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    weight_grs = db.Column(db.Float, nullable=False)
+    shape = db.Column(db.String(50), nullable=False)
+    cavities = db.relationship('Cavity', backref='piece', lazy=True)
+
+    def __init__(self, name, weight_grs, shape):
+        self.weight_grs = weight_grs
+        self.name = name
+        if shape not in SHAPES:
+            raise ValueError(f"the shape {shape} is invalid.")
+        self.shape = shape
